9dc465bca88b8f3f24fc056be55edef977779a01
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-02-19 23:22:26 +0630

    form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
 export default function App(){
   return (
     <>
-      <h1>Chef Claude</h1>
+      <Header />
+      <Main />
     </>
   );
 }
