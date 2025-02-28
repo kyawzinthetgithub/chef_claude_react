38830b65bd25d243e2928022d225e60c98dd1d9b
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-02-28 20:44:15 +0630

    split file for ingredientlist and clude recipe and pass props value & function

---
+++ src/components/IngredientList.jsx
@@ -0,0 +1,22 @@
+export default function IngredientList ({ingredients,getRecipe}){
+    return (
+        <>
+            <section>
+            <h2>Ingredient on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">
+              {ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+            {ingredients.length > 3 && (
+              <div className="get-recipe-container">
+                <div>
+                  <h3>Ready for recipe</h3>
+                </div>
+                <button onClick={getRecipe}>Get Recipe</button>
+              </div>
+            )}
+          </section>
+        </>
+    );
+}
